069ddcbdee4020473b33bb6dac1ee698fd5fccbb
seifediine <seifeddine.elkorichi@hotmail.com>
2022-01-12 17:00:56 +0100

    Header & Footer

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import styles from '../styles/Footer.module.css'
+
+const Footer = () => {
+  const date = new Date()
+  const currentYear = date.getFullYear()
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright &copy; DJ Events, {currentYear}. All Rights Reserved</p>
+      <p>
+        <Link href='/about'>About This Project</Link>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
