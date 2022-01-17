c8dd8406a2ddac1b98c8c606acea0911a081c47d
seifediine <seifeddine.elkorichi@hotmail.com>
2022-01-17 16:08:09 +0100

    fetch events from api with getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-const HomePage = () => {
+const HomePage = ({ events }) => {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
 }
 
 export default HomePage
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+    revalidate: 1,
+  }
+}
